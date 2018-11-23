677f6d9872ab82e67be03278d1973fc8ceaa9bdc
Scott Hadzik <scotthadzik@weber.edu>
2018-11-22 18:57:59 -0700

    Added volatage monitor

--- batteryMonitor.py
+++ batteryMonitor.py
@@ -1,9 +1,29 @@
-from guizero import App, Text
+# from guizero import App, Text
 
-app = App(title="Battery Monitor", layout="grid" )
-batteryMessage      = Text(app, text="Battery Voltage", grid=[0,0])
-temperatureMessage  = Text(app, text="Temperature", grid=[1,0])
+# app = App(title="Battery Monitor", layout="grid" )
+# batteryMessage      = Text(app, text="Battery Voltage", grid=[0,0])
+# temperatureMessage  = Text(app, text="Temperature", grid=[1,0], align="right")
 
-app.display()
+# app.display()
 
-print("Connected to Pi") # Added to Github
+# print("Connected to Pi") # Added to Github
+
+import PCF8591 as ADC
+
+def setup():
+	ADC.setup(0x48)
+
+def loop():
+	while True:
+		print (ADC.read(0))
+		ADC.write(ADC.read(0))
+
+def destroy():
+	ADC.write(0)
+
+if __name__ == "__main__":
+	try:
+		setup()
+		loop()
+	except KeyboardInterrupt:
+		destroy()
