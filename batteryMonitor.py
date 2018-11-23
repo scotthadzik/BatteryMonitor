2d6daebaa91907da0e2f390e8da7428d2c0d0e12
Scott Hadzik <scotthadzik@weber.edu>
2018-11-22 18:34:35 -0700

    fixed arguments for APP

--- batteryMonitor.py
+++ batteryMonitor.py
@@ -1,6 +1,6 @@
 from guizero import App, Text
 
-app = App(title="Battery Monitor" layout="grid" )
+app = App(title="Battery Monitor", layout="grid" )
 batteryMessage      = Text(app, text="Battery Voltage" grid=[0,0])
 temperatureMessage  = Text(app, text="Temperature" grid=[1,0])
 
