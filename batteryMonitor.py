6bade5e9ee8afb177912db4abb26f715c7fe61d3
Scott Hadzik <scotthadzik@weber.edu>
2018-11-22 18:33:03 -0700

    added Title

--- batteryMonitor.py
+++ batteryMonitor.py
@@ -1,6 +1,9 @@
-from guizero import App
+from guizero import App, Text
 
-app = App(title="Battery Monitor")
+app = App(title="Battery Monitor" layout="grid" )
+batteryMessage      = Text(app, text="Battery Voltage" grid=[0,0])
+temperatureMessage  = Text(app, text="Temperature" grid=[1,0])
+
 app.display()
 
 print("Connected to Pi") # Added to Github
