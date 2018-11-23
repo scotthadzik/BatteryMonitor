aeeda22781cd8302e5fbb09dcffaf8222c23f1e8
Scott Hadzik <scotthadzik@weber.edu>
2018-11-22 18:35:31 -0700

    fixed syntax

--- batteryMonitor.py
+++ batteryMonitor.py
@@ -1,8 +1,8 @@
 from guizero import App, Text
 
 app = App(title="Battery Monitor", layout="grid" )
-batteryMessage      = Text(app, text="Battery Voltage" grid=[0,0])
-temperatureMessage  = Text(app, text="Temperature" grid=[1,0])
+batteryMessage      = Text(app, text="Battery Voltage", grid=[0,0])
+temperatureMessage  = Text(app, text="Temperature", grid=[1,0])
 
 app.display()
 
