8c25f421ee3cbf578784334e11749927f88c77e1
Scott Hadzik <scotthadzik@weber.edu>
2018-12-19 22:40:53 -0700

    added destroy

--- sendIPAddress.py
+++ sendIPAddress.py
@@ -5,11 +5,13 @@ import sys
 def setup():
 	IPAddress = '10.0.0.9'
 
+def destroy():
+	print('end')
+
 if __name__ == "__main__":
 	try:
 		setup()
 		print("sms sent")
 		os.system("python send_sms.py") #Using this line in any python program you can send sms. This call the send_sms.py program and runs
 	except KeyboardInterrupt:
-		
-
+		destroy()
