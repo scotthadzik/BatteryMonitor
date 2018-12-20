6506fe53c72756e0cddc738dbfe4388c6f7ad6ae
Scott Hadzik <scotthadzik@weber.edu>
2018-12-19 22:32:15 -0700

    changed space

--- sendIPAddress.py
+++ sendIPAddress.py
@@ -7,10 +7,5 @@ def setup():
                     
 if _name_ == "_main_":     # Program start from here
         setup()
-        try:
-            print("sms sent")
-			os.system("python send_sms.py") #Using this line in any python program you can send sms. This call the send_sms.py program and runs it.
-			break				              		
-		
-        except KeyboardInterrupt:  # When 'Ctrl+C' is pressed, the child program destroy() will be  executed.
-                destroy()
+        print("sms sent")
+        os.system("python send_sms.py") #Using this line in any python program you can send sms. This call the send_sms.py program and runs
