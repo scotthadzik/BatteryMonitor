966a50c09c8b133f318f39a96822c5a2b5bb4cf3
Scott Hadzik <scotthadzik@weber.edu>
2019-08-31 19:19:05 -0600

    message

--- batteryMonitor.py
+++ batteryMonitor.py
@@ -9,7 +9,7 @@ from twilio.rest import Client
 
 # Your Account Sid and Auth Token from twilio.com/console
 # DANGER! This is insecure. See http://twil.io/secure
-account_sid = os.environ['TWILIO_ACTN_SID']
+account_sid = os.environ['TWILIO_ACNT_SID']
 auth_token = os.environ['TWILIO_AUTH_TOKEN']
 client = Client(account_sid, auth_token)
 
