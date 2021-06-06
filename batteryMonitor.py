4bd3cee94e4248acdc97c036da5ca8a47e4ee355
Scott Hadzik <scotthadzik@weber.edu>
2021-06-06 00:17:00 -0600

    signal value

--- batteryMonitor.py
+++ batteryMonitor.py
@@ -104,6 +104,7 @@ def networkStatus():
 	numbers = reg_ex_result.findall(result) # search for the first occurance of numbers
 	first_num= numbers[0].split(',') # split the return list based on coma
 	signal_value = int(first_num[0]) # convert the first set of numbers to integer
+	print (signal_value)
 	if signal_value < 10:	# evaluate quality of the signal
 		return ('marginal')
 	elif signal_value < 14:
