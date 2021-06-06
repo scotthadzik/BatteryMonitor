db3194348e72c74605f601636da164c96f7a38ac
Scott Hadzik <scotthadzik@weber.edu>
2021-06-05 20:07:00 -0600

    fixed duty cycle

--- rgb.py
+++ rgb.py
@@ -49,6 +49,8 @@ def setColor(col):   # For example : col = 0x112233
 	B_val = map(B_val, 0, 255, 0, 100)
 	
 	p_R.ChangeDutyCycle(100-R_val) 
+	p_G.ChangeDutyCycle(100-G_val) 
+	p_B.ChangeDutyCycle(100-B_val) 
 
 def loop():
 	while True:
